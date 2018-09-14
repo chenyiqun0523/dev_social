1f8b0f5647706774ddbca125e79f6ccdd6b71c18
JeffC <chenyiqun0523@gmail.com>
2018-09-13 22:11:50 -0700

    JWT authentication

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://chenyiqun0523:cz620505@ds151382.mlab.com:51382/ign_conn'
+    mongoURI: 'mongodb://chenyiqun0523:cz620505@ds151382.mlab.com:51382/ign_conn',
+    secretOrKey: 'mysecret'
 };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');  
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 
 const users = require('./routes/api/users');
@@ -21,7 +22,12 @@ mongoose
    .then(() => console.log('MongoDB is connected'))
    .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello'));
+//app.get('/', (req, res) => res.send('Hello'));
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
 
 //Use routes
 app.use('/api/users', users);
