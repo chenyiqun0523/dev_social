725f59108febb2f2267cd2fd47cf8357a79f3ae4
JeffC <chenyiqun0523@gmail.com>
2018-10-03 00:59:27 -0700

    getProfileByHandle action; start display profile

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -28,6 +28,26 @@ export const getCurrentProfile = () => dispatch => {
 };
 
 
+// Get profile by handle
+export const getProfileByHandle = (handle) => dispatch => {
+    dispatch(setProfileLoading());  //set loading to true
+    axios.get(`/api/profile/handle/${handle}`)
+        .then(res => 
+           dispatch({
+               type: GET_PROFILE,
+               payload: res.data
+           })
+        )
+        .catch(err =>    //if there is an error, it means there is no profile for this user, which is ok
+                        // just return an empty payload
+          dispatch({
+              type: GET_PROFILE,
+              payload: {}
+          })  
+        );
+};
+
+
 // Create Profile
 export const createProfile = (profileData, history) => dispatch => {
     axios.post('/api/profile', profileData)
