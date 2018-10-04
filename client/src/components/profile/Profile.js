fdd04a6a94f0386f2914bd1be3f737cd412064e4
JeffC <chenyiqun0523@gmail.com>
2018-10-03 23:02:55 -0700

    profile github API

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -17,6 +17,12 @@ class Profile extends Component {
     }
   }
 
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.profile.profile === null && this.props.profile.loading) {
+      this.props.history.push('/not-found');
+    }
+  }
+
   render() {
     const { profile, loading } = this.props.profile;
     let profileContent;
@@ -37,14 +43,16 @@ class Profile extends Component {
           <ProfileHeader profile={profile}/>
           <ProfileAbout profile={profile}/>
           <ProfileCreds education={profile.education} experience={profile.experience}/>
-          <ProfileGithub />
+          {profile.githubusername ? (
+            <ProfileGithub username={profile.githubusername}/>
+          ) : null}
         </div>
       );
     }
 
     return (
         <div className="profile">
-            <div lassName="container">
+            <div className="container">
                 <div className="row">
                     <div className="col-md-12">
                        {profileContent} 
